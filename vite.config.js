9f9f5f89a92999db5a31cc48c6311d3b913d878f
Cristobal Sciutto <cristobal.sciutto@gmail.com>
2026-05-26 15:50:18 -0400

    smaller bundle, split cache

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import react from "@vitejs/plugin-react"
 import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom", "react-router-dom"],
+          supabase: ["@supabase/supabase-js"],
+        },
+      },
+    },
+  },
   plugins: [
     react(),
     VitePWA({
